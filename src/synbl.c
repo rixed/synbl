a7ee03142d5d1b7742103811780249f31d6e777b
Cedric Cellier <cedric.cellier@securactive.net>
2011-02-03 16:12:33 +0100

    The main data structures
    This time we are merely defining two data structures.
    
    The first one is a very basic struct syner which purpose is mainly to
    store an IP address and a port, and which represent a remote client that
    sent us a SYN to this port, SYNs that we are about to measure the rate.
    
    This structure comes with for helping function to allocate, initialize,
    deinitialize and free a struct syner.
    
    As often when programming in C most of the changes are for laborious
    repetitive coding. But at least it is simple and easy to follow.
    
    The second structure, syners, is a hash of those syners, using syner.key
    (ie. IP and port) as the indexing key. Notice that syner.key memset to 0
    in syner constructor so that memcmp on hash keys will not be affected by
    uninitialized padding bytes.
    
    The hash is initialized (empty) when loading the plugin, and carefully
    emptied and destructed at exit. This is because junkie being a dynamic
    program, the plugin can be unloaded and the sniffer continuing its duty,
    or even the same plugin reloaded several times in a loop, so you do not
    want to leak memory at exit.
    
    Next commit we will start performing actual work: receiving traffic
    information and updating the hash of syners!

--- src/synbl.c
+++ src/synbl.c
@@ -19,9 +19,14 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
 #include <junkie/cpp.h>
 #include <junkie/proto/proto.h>
 #include <junkie/tools/cli.h>
+#include <junkie/tools/hash.h>
+#include <junkie/tools/ip_addr.h>
+#include <junkie/tools/mallocer.h>
 
 static unsigned opt_max_syn   = 1;
 static unsigned opt_period    = 1;
@@ -33,16 +38,79 @@ static struct cli_opt options[] = {
     { { "probation", NULL }, true, "Duration (in seconds) of the blacklist",                          CLI_SET_UINT, { .uint = &opt_probation } },
 };
 
+/*
+ * A syner is a client IP and a target port, corresponding to a TCP SYN we have seen.
+ */
+
+struct syner {
+    HASH_ENTRY(syner) entry;
+    struct syner_key {
+        struct ip_addr ip;
+        uint16_t dport;
+    } key;
+    // ...
+};
+
+static HASH_TABLE(syners, syner) syners;
+
+static int syner_ctor(struct syner *syner, struct ip_addr const *ip, uint16_t dport)
+{
+    SLOG(LOG_DEBUG, "Constructing new syner@%p for %s, dport %"PRIu16, syner, ip_addr_2_str(ip), dport);
+
+    memset(syner.key, 0, sizeof(syner.key));    // because we will memcmp keys
+    syner->key.ip = *ip;
+    syner->key.dport = dport;
+
+    HASH_INSERT(&syners, syner, &syner->key, entry);
+
+    return 0;
+}
+
+static struct syner *syner_new(struct ip_addr const *ip, uint16_t dport)
+{
+    MALLOCER(syners);
+    struct syner *syner = MALLOC(syners, sizeof(*syner));
+    if (! syner) return NULL;
+
+    if (0 != syner_ctor(syner, ip, dport)) {
+        FREE(syner);
+        return NULL;
+    }
+
+    return syner;
+}
+
+static void syner_dtor(struct syner *syner)
+{
+    HASH_REMOVE(&syners, syner, entry);
+}
+
+static void syner_del(struct syner *syner)
+{
+    syner_dtor(syner);
+    FREE(syner);
+}
+
+
 void on_load(void)
 {
 	SLOG(LOG_DEBUG, "Loading synbl");
 
     (void)cli_register("synbl", options, NB_ELEMS(options));
+
+    HASH_INIT(&syners, 1000, "syners");
 }
 
 void on_unload(void)
 {
 	SLOG(LOG_DEBUG, "Unloading synbl");
+
+    // Deletes every syners
+    struct syner *syner, *tmp;
+    HASH_FOREACH_SAFE(syner, &syners, entry, tmp) {
+        syner_del(syner);
+    }
+    HASH_DEINIT(&syners);
 
     (void)cli_unregister(options);
 }
