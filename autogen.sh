ecd938c9064978304b6e51af082ee351ba523383
Cedric Cellier <cedric.cellier@securactive.net>
2011-02-03 13:18:22 +0100

    Initial boiler plate
    Introducing SynBlackList
    ------------------------
    
    I was monitoring one of my server this morning and noticed many unsuccessful
    ssh connection attempts in the log. Even when your passwords are strong I feel
    this is still a nuisance. There are many solutions to this problem :
    
    - either change the service port from the well known one to another one, which
      is not always feasible (some ports are well known for good reason).
    
    - use a program that scans the log and automatically firewall connection
      attempts from this IP for a given probation period. It saves your server
    some packets to receive and send and the bother for sshd to perform a full
    blown handshake.
    
    - any other solution that I'm unaware of.
    
    This gave me the idea of a nice tutorial on how to write a plugin for Junkie.
    
    As ideas usually come in pair, I also imagined to use git as a kind of generic
    literate programming tool. Thus these unusually long commit messages.
    
    So for a start I just created all the required boiler plate that you are
    probably already too familiar with :
    
    - autogen.sh : call all the autotools required to lift the versionned
      sourcetree into a development sourcetree
    
    - configure.ac : preparing to use automake, autoheader, libtool and friends,
      and checking that the junkie header files are indeed present somewhere
    
    - Makefile.am : main automakefile, just skipping to...
    
    - src/Makefile.am : building the synbl.so plugin from...
    
    - src/synbl.c, the plugin itself which performs nothing but to log a
      message in (de)initialization functions when called by junkie.
    
    - .gitignore : informing git to ignore all generated files
    
    Next commit we will sketch the internal of the plugin.

---
+++ autogen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Usage sh -x ./autogen.sh
+
+set -e
+
+mkdir -p build-aux
+libtoolize --force
+aclocal
+autoheader
+automake --add-missing --foreign
+autoconf
+
+echo "Now run configure and make."
---
+++ src/synbl.c
@@ -0,0 +1,33 @@
+// -*- c-basic-offset: 4; c-backslash-column: 79; indent-tabs-mode: nil -*-
+// vim:sw=4 ts=4 sts=4 expandtab
+/* Copyright 2010, SecurActive.
+ *
+ * This file is part of Junkie.
+ *
+ * Junkie is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Junkie is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Junkie.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <junkie/cpp.h>
+#include <junkie/proto/proto.h>
+
+void on_load(void)
+{
+	SLOG(LOG_DEBUG, "Loading synbl");
+}
+
+void on_unload(void)
+{
+	SLOG(LOG_DEBUG, "Unloading synbl");
+}
