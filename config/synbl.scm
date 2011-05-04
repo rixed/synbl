453fe57df36cd66e9fbc93acb2c0406d0806384c
Cedric Cellier <cedric.cellier@securactive.net>
2011-05-04 15:00:39 +0200

    First usable version --------------------
    If by usable we mean that the program actually does something.
    
    We only changed config/synbl.scm here, first to implement synbl-ban and
    synbl-unban to use iptables to blacklist a given IP address. So if you run a
    Linux OS this plugin should run and perform the required (un)banishments of
    peers.
    
    The scm file is also altered to load the plugin itself so that from junkie
    command line you merely have to load this config file (assuming the library is
    accessible to the loader).  So for instance, to run it from the build
    directory, use:
    
    LD_LIBRARY_PATH=src/.libs junkie -c config/synbl.scm
    
    with additional options as suit your needs.
    
    You should notice two important annoyances:
    
    - we do not distinguish between incoming and outgoing traffic (and packets just
      passing by, actually), so that we may end up banishing our own host;
    
    - many legitimate applications will soon get banished.
    
    We will address these two issues in the two following commits.

--- config/synbl.scm
+++ config/synbl.scm
@@ -1,8 +1,19 @@
 ; vim:syntax=scheme expandtab
-;;; This file will define the two guile function called by the plugin,
+;;; This file defines the two guile functions called by the plugin,
 ;;; one to blacklist an IP and the other one the reenable it.
 (define-module (junkie synbl))
+(use-modules (guile-user))
+(if (not (load-plugin "synbl.so"))
+    (begin (display "Cannot load synbl.so plugin\n")
+           (exit 1)))
 
-(define (synbl-ban ip dport)
-  (simple-format #t "Banning IP ~a, dport ~a\n" ip dport))
+(define (do-iptables cmd ip dport)
+  (system (simple-format #f "iptables ~a INPUT -s ~a/32 -p tcp --syn --dport ~a -d 0/0 -j DROP" cmd ip dport)))
 
+(define-public (synbl-ban ip dport)
+  (simple-format #t "Banning IP ~a, dport ~a~%" ip dport)
+  (do-iptables "-A" ip dport))
+
+(define-public (synbl-unban ip dport)
+  (simple-format #t "Unbanning IP ~a, dport ~a~%" ip dport)
+  (do-iptables "-D" ip dport))
